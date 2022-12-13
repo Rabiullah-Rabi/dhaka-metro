1c09071356f0700c9c3134e296e6682d29865a70
Mr-Robot-101 <git.sujon@gmail.com>
2022-12-13 18:04:33 +0600

    Table Added

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Banner from './banner/banner'
+import NextTrain from './NextRail/nextTrain'
 
 export default function Home() {
   Head('Dhaka Metro')
@@ -12,6 +13,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Banner></Banner>
+      <NextTrain></NextTrain>
     </div>
   )
 }
